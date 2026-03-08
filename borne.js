c13682f2d9867d43dfce325b5e8d22aee79c4d7e
mishrayush15 <ayushmishra780193@gmail.com>
2026-03-08 20:06:34 +0530

    cors update

--- borne.js
+++ borne.js
@@ -19,7 +19,13 @@ initFirebase();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://air-borne-n28j.vercel.app',
+  ],
+  credentials: true,
+}));
 app.use(express.json({ limit: "5mb" }));
 app.use("/uploads", express.static("uploads"));
 
